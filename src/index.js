8dcb5ca1f115a9f9b2e947c6c5e22124595e6c37
MuAnto <antonio.murolo@buzzoole.com>
2021-04-29 07:49:22 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+
+
+class SimpleComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = /*html*/`
+    <style>
+      div { 
+        color: red;
+        font-family: Arial;
+        font-size: 18px;
+        }
+    </style>
+    <div>I am a simple component</button>
+  `;
+  }
+ 
+}
+
+/* Custom elements */
+customElements.define("simple-component", SimpleComponent);
